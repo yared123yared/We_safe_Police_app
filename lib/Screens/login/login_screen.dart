ec76acf8cf934017df2d245a5011a30592a50ce0
fasikawkn <fasikawkn@gmail.com>
2022-03-28 22:52:30 +0300

    login page

--- lib/Screens/login/login_screen.dart
+++ lib/Screens/login/login_screen.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class PoliceLoginpage extends StatefulWidget {
+  const PoliceLoginpage({ Key? key }) : super(key: key);
+
+  @override
+  State<PoliceLoginpage> createState() => _PoliceLoginpageState();
+}
+
+class _PoliceLoginpageState extends State<PoliceLoginpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
